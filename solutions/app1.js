88081d196b11b1ebe496dc2643006a8950108479
zsuzsasovari <105643090+zsuzsasovari@users.noreply.github.com>
2022-08-17 14:22:44 +0200

    kész

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,5 +1,13 @@
+const getSecondsOfThisYearUntilNow = () => {
+    let difference = (new Date('August 31') - new Date('January 1')) / 1000 ;
+    return Math.round(difference);
+}
 
 
 
-
-// export default getSecondsOfThisYearUntilNow;
+export default getSecondsOfThisYearUntilNow;
+// ## 1. feladat
+// - Fájl: `solutions/app1.js`
+// - Függvény neve: `getSecondsOfThisYearUntilNow`
+// - Írj egy függvényt, amelynek a visszatérési értéke az adott év 01. 01-től 08. 31-ig eltelt másodperceinek a száma! 
+// - _Kerekítést használj, amikor osztasz 1000-rel! (Math.round())_
--- solutions/app3.js
+++ solutions/app3.js
@@ -1,7 +1,28 @@
+const customDateFormats = (date) => {
+    return {
+        'short': date.toLocaleDateString("hu-hu", {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+        'long': date.toLocaleDateString("hu-hu", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        }),
+      };
+    }
 
+export default customDateFormats;
 
-
-
-
-
-// export default customDateFormats;
+// ## 3. feladat
+// - Fájl: `solutions/app3.js`
+// - Függvény neve: `customDateFormats`
+// - Írj egy függvényt, amely paraméterként egy Date objektumot kap, a visszatérési értéke pedig egy objektum két tulajdonsággal, amely a következő formátumban tartalmazza a paraméterként kapott dátumot:
+// - `short`: 2020. jan. 11. 14:20 (tehát évszám, a hónap neve röviden magyarul, kisbetűkkel a nap száma, majd az idő a másodperc nélkül)
+// - `long`: 2020. január 11. 14:20:10 (tehát évszám, a hónap neve magyarul, a nap száma, majd az idő)
